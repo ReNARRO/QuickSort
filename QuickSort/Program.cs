ca079ff314b37ebe592c8c8a66536b0d96fa5d28
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2022-10-19 17:21:46 +0700

    menambahkan fungsi if dengan perbadingan i kurang dari j

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -81,6 +81,12 @@ namespace QuickSort
                 }
                 cmp_count++;
 
+                if (i < j) //if the greater element is on the left of the element
+                {
+                    //swap the element at index i whit the element at index j
+                    swap(i,j);
+                    mov_count++;
+                }
 
             }
         }
